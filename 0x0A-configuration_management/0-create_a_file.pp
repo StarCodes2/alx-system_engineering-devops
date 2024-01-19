5d281d6819ee52a27453b5fd2d8e6235c67a7839
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-19 14:00:55 -0800

    To create a file in /tmp.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+file { 'tmp/school':
+  ensure => 'file',
+  owner  =>  'www-data',
+  group  => 'www-data',
+  mode   => '0744'
+}
+
+exec { 'file content':
+  command => '/usr/bin/echo I love Puppet > tmp/school'
+  require => File['tmp/school']
+}
