02b3d4d64be7573ad033be23838318d5d021cf6d
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-22 04:05:41 -0800

    Update configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,4 @@
 #!/usr/bin/pup
-exec { 'first line':
-  command => '/bin/echo "Host *" > ~/.ssh/config',
-  require => File['~/.ssh/config'],
-}
-
-exec { 'second line':
-  command => '/bin/echo "    IdentityFile ~/.ssh/school" >> ~/.ssh/config',
-  require => Exec['first line'],
-}
-
-exec { 'third line':
-  command => '/bin/echo "    PasswordAuthentication no" >> ~/.ssh/config',
-  require => Exec['second line'],
+exec { 'append to /etc/ssh/ssh_config':
+  command => '/bin/echo "Host *\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
 }
