61840bfd732888c197e2da626857175f75e8c519
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-23 07:29:57 -0800

    Using puppet to install and configure nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,61 @@
+#!/usr/bin/pup
+# Install Nginx, configure it to listen on port 80 and returns a page
+# containing the string 'Hello World!' when queried with curl and redirect
+# /redirect_me to another page
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+exec { 'install nginx':
+  command => '/usr/bin/apt-get -y install nginx',
+  require => Exec['apt-get update'],
+}
+
+file { '/etc/nginx/html':
+  ensure  => 'directory',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => Exec['install nginx'],
+}
+
+exec { 'index':
+  command => '/bin/echo "Hello World!" > /etc/nginx/html/index.html',
+  require => File['/etc/nginx/html'],
+}
+
+exec { '404':
+  command => "/bin/echo Ceci n'est pas une page > /etc/nginx/html/404.html",
+  require => File['/etc/nginx/html'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => |
+"server {
+	listen	80 default_server;
+	listen	[::]:80 default_server;
+	root	/etc/nginx/html;
+	index	index.html index.htm;
+
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /etc/nginx/html;
+		internal;
+	}
+}",
+  notify  => Service['nginx'],
+  require => Exec['install nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
