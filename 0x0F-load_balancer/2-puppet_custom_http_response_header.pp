f64ce237cfd83b21b01c6bb6b66fe993f9bb3150
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-02-04 11:46:20 -0800

    BUG: not executing

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,52 +1,50 @@
-#!/usr/bin/pup
 # Install and configure Nginx so that its HTTP response comtains a custom header
-exec { 'update':
-  command => '/usr/bin/apt-get update',
-}
 
-exec { 'install':
-  command => '/usr/bin/apt-get install -y nginx',
-  require => Exec['update'],
-}
+$config_nginx = "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm;
+        add_header X-Served-By ${HOSTNAME};
 
-exec { 'create dir':
-  command => '/usr/bin/mkdir -p /var/www/html',
-  require => 'Exec[install'],
-}
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
 
-exec { 'index':
-  command => 'echo "Hello World!" > /var/www/html/index.html',
-  require => Exec['create dir'],
-}
+        error_page 404 /404.html;
+        location /404 {
+                root /var/www/html;
+                internal;
+        }
+}"
 
-exec { '404':
-  command => "/usr/bin/echo 'Ceci n'est pas une page' > /var/www/html/404.html",
-  require => Exec['create dir'],
-}
-exec { 'config':
-  command => |
-"/usr/bin/printf %s \"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm;
-	add_header X-Served-By ${HOSTNAME};
+exec { 'apt-get update':
+  path   => '/usr/bin/:/usr/local/bin/:/bin/'
+} ->
 
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
+exec { 'apt-get install -y nginx':
+  path   => '/usr/bin/:/usr/local/bin/:/bin/'
+} ->
 
-	error_page 404 /404.html;
-	location /404 {
-		root /var/www/html;
-		internal;
-	}
-}
-\" > /etc/nginx/sites-available/default",
-  require => Exec['404'],
-}
+exec { 'mkdir -p /var/www/html':
+  path   => '/usr/bin/:/usr/local/bin/:/bin/'
+} ->
 
-exec { 'restart':
-  command => 'service nginx restart',
-  require => Exec['config'],
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!'
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page"
+} ->
+
+exec { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $config_nginx
+} ->
+
+exec { 'nginx restart':
+  path   => '/etc/init.d/'
 }
