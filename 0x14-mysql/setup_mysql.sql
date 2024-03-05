847c4be1c603db41b6039e704198dff1a6e9fdda
Ogunewu Ezekiel Anuoluwapo <55100341+StarCodes2@users.noreply.github.com>
2024-03-05 15:05:19 +0100

    Delete 0x14-mysql directory

--- 0x14-mysql/setup_mysql.sql
+++
@@ -1,9 +0,0 @@
--- Creates a database hbnb_dev_db, user 'hbnb_dev' and sets the user's privileges
-CREATE DATABASE IF NOT EXISTS tyrell_corp;
-USE tyrell_corp;
-CREATE TABLE IF NOT EXISTS nexus6 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
-INSERT INTO nexus6 (name) VALUES ("Leon");
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
