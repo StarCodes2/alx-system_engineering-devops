6f887a46102ee7f69ef4c979941b60c1c794a684
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-05-07 16:05:15 +0000

    To return a list containing the titles of all hot articles for a given subreddit.

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Function to query and return a list of all hot articles in a
+    given subreddit."""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=""):
+    """ Returns the titles of all hot articles of a given subreddit. """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    param = {
+        "count": count,
+        "after": after,
+        "limit": 100
+    }
+    headers = {
+        "User-Agent": "linux:api.request.red:v1.0.0 (by /u/Muted_Reason6144)"
+    }
+    res = requests.get(url, headers=headers, params=param,
+                       allow_redirects=False)
+    if res.status_code == 404:
+        return None
+
+    data = res.json().get("data")
+    count += data.get("dist")
+    after = data.get("after")
+    for child in data.get("children"):
+        hot_list.append(child.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, count, after)
+    return hot_list
